73885bf0e99b618f3e3272d50d206db0a1110563
nguyen.tu.anh <nguyen.tu.anh@framgia.com>
2018-06-05 10:14:00 +0700

    [Tuanh][TabBar]Create tab for tabbar

---
+++ FBNewFeeds/FBNewFeeds/Configs/AppColor.swift
@@ -0,0 +1,17 @@
+//
+//  AppColor.swift
+//  FBNewFeeds
+//
+//  Created by nguyen.tu.anh on 5/30/18.
+//  Copyright © 2018 nguyen.tu.anh. All rights reserved.
+//
+
+import UIKit
+
+class AppColor {
+
+    static func blueColor() -> UIColor {
+        return UIColor.fromHex("254A85")
+    }
+
+}
---
+++ FBNewFeeds/FBNewFeeds/Modules/Notify/NotifyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NotifyViewController.swift
+//  FBNewFeeds
+//
+//  Created by nguyen.tu.anh on 5/30/18.
+//  Copyright © 2018 nguyen.tu.anh. All rights reserved.
+//
+
+import UIKit
+
+class NotifyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
