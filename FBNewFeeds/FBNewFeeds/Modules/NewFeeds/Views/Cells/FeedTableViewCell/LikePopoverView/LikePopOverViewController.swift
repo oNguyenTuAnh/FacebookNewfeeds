8acae87b926eb15415103bc71c665a59f128bedd
nguyen.tu.anh <nguyen.tu.anh@framgia.com>
2018-06-12 13:53:54 +0700

    [Tuanh][Newsfeed] Action like

---
+++ FBNewFeeds/FBNewFeeds/Modules/NewFeeds/Views/Cells/FeedTableViewCell/LikePopoverView/LikePopOverViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LikePopOverViewController.swift
+//  FBNewFeeds
+//
+//  Created by nguyen.tu.anh on 6/12/18.
+//  Copyright © 2018 nguyen.tu.anh. All rights reserved.
+//
+
+import UIKit
+
+class LikePopOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
